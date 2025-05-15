76bbb43393d28e58d628727aa5afa334991bf97e
Yosef <joseph88.me@gmail.com>
2025-05-15 20:01:39 +0100

    future is loading

--- EXAM00/aff_z.c/aff_z.c
+++ EXAM00/aff_z.c/aff_z.c
@@ -1,26 +1,22 @@
+#include <unistd.h>
 
-
-    #include <unistd.h>
-
-    int main(int argc , char **argv){
-    int i;
-    // start of the argument
-    i = 0;
-    if (argc > 1){
-
-        while( argv[1][i]){
-            write(1,&argv[1][i], 1);
-            i++;
-        }
-        write(1,"\n",1);
-
-
+int main( int argc ,char **argv){
+ int i = 0;
+//scan for z
+ if ( argc == 2){
+ while ( argv[1][i]){
+    if(argv[1][i]=='z'){
+        write(1,"z\n",2);
+        return 0;// to stop
     }
-    
+    i++;
+ }
+ // if we didnt found z in the string but we still have an argument
+ write(1,"z\n",2);
+ }
+// if we have only one argument which is ./a.out then print z
+ else {
+    write ( 1 , "z\n",2);
+ }
 
-    else if( argc == 1){
-        write(1,"\n",1);
-    }
-
-
-    }
+}
