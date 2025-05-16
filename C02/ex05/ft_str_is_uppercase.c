1f7af682477c9e1cbf42bb05d70bc64a08a70e18
Yosef <joseph88.me@gmail.com>
2025-05-16 22:03:12 +0100

    future is loading

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int ft_str_is_uppercase(char *str)
+{
+    int i;
+    i = 0;
+    while ( str[i] != '\0')
+    {
+        if( str[i] >= 65 && str[i] <= 90){
+            //nothing here
+        }
+
+        else{
+            return 0;
+        }
+        i++;
+
+
+
+    }
+    return 1;
+
+
+
+
+
+}
